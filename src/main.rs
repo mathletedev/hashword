fb91128a7f81afd7ca2e65b5533108950ca8c2c3
Neal Wang <nealwang.sh@protonmail.com>
2024-07-29 16:50:20 -0700

    feat: use default rustfmt config

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,15 @@ use rpassword::prompt_password;
 use std::process::Command;
 
 const CONFIG: Config = Config {
-	variant: Variant::Argon2id,
-	version: Version::Version13,
-	mem_cost: 65536,
-	time_cost: 4,
-	lanes: 8,
-	thread_mode: ThreadMode::Parallel,
-	secret: &[],
-	ad: &[],
-	hash_length: 46,
+    variant: Variant::Argon2id,
+    version: Version::Version13,
+    mem_cost: 65536,
+    time_cost: 4,
+    lanes: 8,
+    thread_mode: ThreadMode::Parallel,
+    secret: &[],
+    ad: &[],
+    hash_length: 46,
 };
 const LOWERCASE: &str = "abcdefghijklmnopqrstuvwxyz";
 const UPPERCASE: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -19,50 +19,50 @@ const NUMERIC: &str = "0123456789";
 const SPECIAL: &str = "!@#$%^&*";
 
 fn main() {
-	let seed = prompt_password("Enter seed: ").unwrap();
-	let key = prompt_password("Enter key: ").unwrap();
+    let seed = prompt_password("Enter seed: ").unwrap();
+    let key = prompt_password("Enter key: ").unwrap();
 
-	let hash = hash_raw(key.to_lowercase().as_bytes(), seed.as_bytes(), &CONFIG).unwrap();
+    let hash = hash_raw(key.to_lowercase().as_bytes(), seed.as_bytes(), &CONFIG).unwrap();
 
-	let characters = String::new() + LOWERCASE + UPPERCASE + NUMERIC + SPECIAL;
+    let characters = String::new() + LOWERCASE + UPPERCASE + NUMERIC + SPECIAL;
 
-	let mut res = hash
-		.get(0..16)
-		.unwrap()
-		.iter()
-		.map(|e| characters.as_bytes()[*e as usize % characters.chars().count()] as char)
-		.collect::<String>();
+    let mut res = hash
+        .get(0..16)
+        .unwrap()
+        .iter()
+        .map(|e| characters.as_bytes()[*e as usize % characters.chars().count()] as char)
+        .collect::<String>();
 
-	let mut pos: usize = 0;
+    let mut pos: usize = 0;
 
-	hash.get(16..46)
-		.unwrap()
-		.iter()
-		.enumerate()
-		.for_each(|(i, e)| {
-			if i % 2 == 0 {
-				pos = *e as usize % res.chars().count();
-				return;
-			}
+    hash.get(16..46)
+        .unwrap()
+        .iter()
+        .enumerate()
+        .for_each(|(i, e)| {
+            if i % 2 == 0 {
+                pos = *e as usize % res.chars().count();
+                return;
+            }
 
-			let charset = match i {
-				0..=15 => LOWERCASE,
-				16..=23 => UPPERCASE,
-				24..=27 => NUMERIC,
-				28..=29 => SPECIAL,
-				_ => unreachable!(),
-			};
+            let charset = match i {
+                0..=15 => LOWERCASE,
+                16..=23 => UPPERCASE,
+                24..=27 => NUMERIC,
+                28..=29 => SPECIAL,
+                _ => unreachable!(),
+            };
 
-			res.replace_range(
-				pos..pos + 1,
-				&(charset.as_bytes()[*e as usize % charset.len()] as char).to_string(),
-			);
-		});
+            res.replace_range(
+                pos..pos + 1,
+                &(charset.as_bytes()[*e as usize % charset.len()] as char).to_string(),
+            );
+        });
 
-	println!("{}", res);
+    println!("{}", res);
 
-	Command::new("wl-copy")
-		.arg(res)
-		.status()
-		.expect("Failed to copy to clipboard");
+    Command::new("wl-copy")
+        .arg(res)
+        .status()
+        .expect("Failed to copy to clipboard");
 }
